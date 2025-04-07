5b62576eed41ad401be9dac83f462a073cbec8c9
makaron <nicothemuffin@gmail.com>
2025-04-07 14:19:58 +0200

    FINALE

--- AdressBook/MainPage.xaml.cs
+++ AdressBook/MainPage.xaml.cs
@@ -1,36 +1,42 @@
 ﻿using System.Collections.ObjectModel;
+
 namespace AdressBook {
     using MVVM.Models;
     public partial class MainPage : ContentPage {
         ObservableCollection<personData> census { get; set; } = new ObservableCollection<personData>();
         ObservableCollection<personData> searchResults { get; set; } = new ObservableCollection<personData>();
+
         public MainPage() {
             InitializeComponent();
+
             AdressBook.ItemsSource = census;
-            AdressBook.BindingContext = census;
+            AdressBook.BindingContext = this;
+
             AdressBookSearch.ItemsSource = searchResults;
-            AdressBookSearch.BindingContext = searchResults;
+
+            SearchList.BindingContext = this;
+
             editForm.IsVisible = false;
             SearchList.IsVisible = false;
         }
+
         private void addNewEntry(object sender, EventArgs e) {
-            string surname = entrySurname.Text;
-            string tel = entryTel.Text;
             if(string.IsNullOrEmpty(entryName.Text) || string.IsNullOrEmpty(entrySurname.Text) || string.IsNullOrEmpty(entryTel.Text)) {
                 DisplayAlert("Error", "Check if values are correct", "Ok");
                 return;
             }
-                
 
-            census.Add(
-                new personData {
-                    FirstName = entryName.Text,
-                    LastName = entrySurname.Text,
-                    Tel = entryTel.Text
-                }
-            );
-            entryName.Text = ""; entrySurname.Text = ""; entryTel.Text = ""; 
+            census.Add(new personData {
+                FirstName = entryName.Text,
+                LastName = entrySurname.Text,
+                Tel = entryTel.Text
+            });
+
+            entryName.Text = ""; entrySurname.Text = ""; entryTel.Text = "";
             entryName.Focus();
+
+            SearchList.IsVisible = false;
+            AdressBook.IsVisible = true;
         }
 
         private int index = -1;
@@ -53,35 +59,48 @@ namespace AdressBook {
                 editSurname.Text = selectedItem.LastName;
                 editTel.Text = selectedItem.Tel;
             }
+
             AdressBook.SelectedItem = null;
         }
 
         private void editUser(object sender, EventArgs e) {
-            census[index].FirstName = editName.Text;
-            census[index].LastName = editSurname.Text;
-            census[index].Tel = editTel.Text;
+            if(index >= 0 && index < census.Count) {
+                var editedPerson = census[index];
+                editedPerson.FirstName = editName.Text;
+                editedPerson.LastName = editSurname.Text;
+                editedPerson.Tel = editTel.Text;
 
-            editName.Text = "";
-            editSurname.Text = "";
-            editTel.Text = "";
+                census[index] = editedPerson;
 
-            census.Add(census[index]);
-            census.RemoveAt(index);
+                editName.Text = "";
+                editSurname.Text = "";
+                editTel.Text = "";
 
-            editForm.IsVisible = false;
-            AdressBook.IsVisible = true;
-            addForm.IsVisible = true;
-            searchBar.IsVisible = true;
+                editForm.IsVisible = false;
+                AdressBook.IsVisible = true;
+                addForm.IsVisible = true;
+                searchBar.IsVisible = true;
+                SearchList.IsVisible = false;
+            }
         }
 
-        private void Search(object sender, EventArgs e) {
-            SearchList.IsVisible = true;
-            string result = searchBar.Text.ToLower();
-
-            //szukaj syckiego
+        private void OnSearchBarTextChanged(object sender, TextChangedEventArgs e) {
+            dynamic filteredResults;
+            string searchText = e.NewTextValue.ToLower();
             searchResults.Clear();
-            foreach(personData person in census) {
-                searchResults.Add(person);
+
+            if(string.IsNullOrEmpty(searchText)) {
+                SearchList.IsVisible = false;
+                AdressBook.IsVisible = true;
+            } else {
+                filteredResults = census.Where(person => person.FirstName.ToLower().Contains(searchText) ||
+                                                             person.LastName.ToLower().Contains(searchText) ||
+                                                             person.Tel.ToLower().Contains(searchText));
+
+                foreach(var person in filteredResults) searchResults.Add(person);
+
+                SearchList.IsVisible = true;
+                AdressBook.IsVisible = false;
             }
         }
     }
